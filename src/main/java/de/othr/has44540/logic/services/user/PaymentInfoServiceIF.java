15bec4c93915b9ef517769cfdb773627bc066531
Valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-01-08 18:48:09 +0100

    Implementing paymentInfoServiceIF

--- src/main/java/de/othr/has44540/logic/services/user/PaymentInfoServiceIF.java
+++ src/main/java/de/othr/has44540/logic/services/user/PaymentInfoServiceIF.java
@@ -3,9 +3,10 @@ package de.othr.has44540.logic.services.user;
 import de.othr.has44540.logic.services.auth.token.AuthToken;
 import de.othr.has44540.persistance.entities.user.paymentInformation.AbstractPaymentMethod;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface PaymentInfoServiceIF {
+public interface PaymentInfoServiceIF extends Serializable{
 
     List<AbstractPaymentMethod> getPaymentMethods(AuthToken authToken);
 }
---
+++ src/main/java/de/othr/has44540/logic/services/user/PaymentInfoServiceImpl.java
@@ -0,0 +1,26 @@
+package de.othr.has44540.logic.services.user;
+
+import de.othr.has44540.logic.services.auth.LoginInterceptor.CheckLogin;
+import de.othr.has44540.logic.services.auth.token.AuthToken;
+import de.othr.has44540.persistance.entities.user.paymentInformation.AbstractPaymentMethod;
+
+import javax.enterprise.context.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@SessionScoped
+public class PaymentInfoServiceImpl implements PaymentInfoServiceIF {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    @CheckLogin
+    public List<AbstractPaymentMethod> getPaymentMethods(AuthToken authToken) {
+        TypedQuery<AbstractPaymentMethod> paymentMethodQuery = em
+                .createQuery("SELECT pm FROM AbstractPaymentMethod AS pm", AbstractPaymentMethod.class);
+        return paymentMethodQuery.getResultList();
+    }
+}
