e6b71ad5e8c81a5feaec79f9ff0ec322cf5b1810
Valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-01-11 09:52:02 +0100

    Creating probable needed models

---
+++ src/main/java/de/othr/has44540/ui/accounts/DonationModel.java
@@ -0,0 +1,8 @@
+package de.othr.has44540.ui.accounts;
+
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+public class DonationModel implements Serializable {
+}
---
+++ src/main/java/de/othr/has44540/ui/utils/AuthModel.java
@@ -0,0 +1,8 @@
+package de.othr.has44540.ui.utils;
+
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+public class AuthModel implements Serializable {
+}
