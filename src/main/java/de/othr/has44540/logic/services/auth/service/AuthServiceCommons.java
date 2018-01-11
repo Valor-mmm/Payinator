435af39a467b7436031bb71e481a5f43f7da81d8
Valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-01-11 13:22:29 +0100

    Throw error on invalid email

--- src/main/java/de/othr/has44540/logic/services/auth/service/AuthServiceCommons.java
+++ src/main/java/de/othr/has44540/logic/services/auth/service/AuthServiceCommons.java
@@ -1,5 +1,7 @@
 package de.othr.has44540.logic.services.auth.service;
 
+import de.othr.has44540.logic.services.exceptions.auth.AuthException;
+import de.othr.has44540.logic.services.exceptions.auth.InvalidLoginDataException;
 import de.othr.has44540.persistance.entities.user.personalData.Email;
 import org.jetbrains.annotations.NotNull;
 
@@ -9,8 +11,16 @@ import javax.persistence.TypedQuery;
 public class AuthServiceCommons {
 
     public static Email queryEmail(@NotNull String email, @NotNull EntityManager em) {
-        String localPart = Email.getLocalPart(email);
-        String domain = Email.getDomain(email);
+        String localPart = null;
+        String domain = null;
+        try {
+            localPart = Email.getLocalPart(email);
+            domain = Email.getDomain(email);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidLoginDataException("Invalid email", "The email you entered did not match an email format.",
+                                                true, false);
+        }
+
         TypedQuery<Email> emailQuery = em
                 .createQuery("SELECT e FROM Email AS e WHERE e.localPart = :localPart AND e.domain = :domain",
                              Email.class);
