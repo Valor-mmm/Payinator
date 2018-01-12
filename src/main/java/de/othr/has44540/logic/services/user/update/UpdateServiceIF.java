da5462a6eae126b076f7e46d91cc315ef66c4e13
Valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-01-12 14:46:24 +0100

    Created base for an update service to update a user based on the session token and a current user object. If the user object is not available, a create user will be executed

--- src/main/java/de/othr/has44540/logic/services/user/update/UpdateServiceIF.java
+++ src/main/java/de/othr/has44540/logic/services/user/update/UpdateServiceIF.java
@@ -1,6 +1,14 @@
 package de.othr.has44540.logic.services.user.update;
 
+import de.othr.has44540.logic.services.exceptions.auth.AuthException;
+import de.othr.has44540.persistance.entities.user.AbstractUser;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.Serializable;
 
-public interface UpdateServiceIF extends Serializable{
+public interface UpdateServiceIF extends Serializable {
+
+    boolean updateUser(@NotNull String sessionToken, AbstractUser user) throws AuthException;
+
+    boolean createUser(@NotNull String sessionToken) throws AuthException;
 }
--- src/main/java/de/othr/has44540/logic/services/user/update/UpdateServiceImpl.java
+++ src/main/java/de/othr/has44540/logic/services/user/update/UpdateServiceImpl.java
@@ -1,4 +1,21 @@
 package de.othr.has44540.logic.services.user.update;
 
-public class UpdateServiceImpl {
+import de.othr.has44540.logic.services.exceptions.auth.AuthException;
+import de.othr.has44540.persistance.entities.user.AbstractUser;
+import org.jetbrains.annotations.NotNull;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UpdateServiceImpl implements UpdateServiceIF {
+
+    @Override
+    public boolean updateUser(@NotNull String sessionToken, AbstractUser user) throws AuthException {
+        return false;
+    }
+
+    @Override
+    public boolean createUser(@NotNull String sessionToken) throws AuthException {
+        return false;
+    }
 }
